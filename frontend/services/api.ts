4c46e030a04d12abddf14995210e03b608dad0c0
Varshini <varshinishanmug@gmail.com>
2026-06-06 11:14:52 +0530

    Configure production backend URL

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// Ensure this matches the port your FastAPI terminal displays on startup
-const API_BASE_URL = 'http://127.0.0.1:8000'; 
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+const API_BASE_URL =  process.env.NEXT_PUBLIC_API_URL; 
 
 export const analyzeResume = async (
   file: File, 
